eff1f866aa9dc11b4f4e86f31658c2bfae6a9786
Oliver <oliver.stache@t-systems.com>
2021-01-13 11:57:32 +0100

    feat: Control commands to sonos via node-sonos added.

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
 const WebSocket = require('ws')
+const { Sonos } = require('sonos')
+
 
 const deHost = process.env.DECONZ_WS_HOST || '192.168.1.50'
 const dePort = process.env.DECONZ_WS_PORT || 443
 const switch_id = process.env.DECONZ_SWITCH_ID || 6 // SYMFONISK
 
-var clockwiseStart = 0
-var counterClockwiseStart = 0
+const zpHost = process.env.ZP_HOST || '192.168.1.3'
+const zpVolFactor = process.env.ZP_VOLFACTOR || 150 // factor used to convert from millis => volume adjustment delta
 
+var tsClockwiseStart = 0
+var tsCounterClockwiseStart = 0
+
+// init sonos player
+const player = new Sonos(zpHost);
+
+// init listening to deconz
 const ws = new WebSocket('ws://' + deHost + ':' + dePort)
 
 console.log('Start listening...')
 
-ws.onmessage = handleBridgeEvent
 // IKEA-SYMFONISK-REMOTE https://dresden-elektronik.github.io/deconz-rest-doc/endpoints/sensors/button_events/#ikea-symfonisk-remote
-function handleBridgeEvent(msg) {
+ws.onmessage = (msg) => {
     try {
         var evt = JSON.parse(msg.data)
     }
     catch (e) {
-        console.log("Cannot parse because data is not is proper json format.")
+        console.error("Cannot parse because data is not is proper json format.")
         return
     }
 
@@ -26,45 +34,40 @@ function handleBridgeEvent(msg) {
         try {
             switch (evt.state.buttonevent) {
                 case 1002:
-                    console.log("Button 1 - Short release")
+                    console.log("Toggle playback")
+                    player.togglePlayback()
                     break
                 case 1004:
-                    console.log("Button 1 - Double press")
+                    console.log("Next title in queue")
+                    player.next()
                     break
                 case 1005:
-                    console.log("Button 1 - Triple press")
+                    console.log("Previous title in queue")
+                    player.previous()
                     break
                 case 2001:
-                    // console.log("Rotate clockwise - Hold")
-                    clockwiseStart = Date.now()
+                    tsClockwiseStart = Date.now()
                     break
                 case 2003:
-                    // console.log("Rotate clockwise - Long release")
-                    duration = Date.now() - clockwiseStart
-                    console.log("Rotate clockwise: %d", duration)
+                    duration = Date.now() - tsClockwiseStart
+                    console.log("volume ∂: %d", duration / zpVolFactor)
+                    player.adjustVolume(duration / zpVolFactor)
+                    player.getVolume().then((vol) => console.log("Volume: %d", vol))
                     break
                 case 3001:
-                    // console.log("Rotate counter clockwise - Hold")
-                    counterClockwiseStart = Date.now()
+                    tsCounterClockwiseStart = Date.now()
                     break
                 case 3003:
-                    // console.log("Rotate counter clockwise - Long release")
-                    duration = Date.now() - counterClockwiseStart
-                    console.log("Rotate counter clockwise: %d", duration)
+                    duration = Date.now() - tsCounterClockwiseStart
+                    console.log("volume ∂: %d", -1 * duration / zpVolFactor)
+                    player.adjustVolume(-1 * duration / zpVolFactor)
+                    player.getVolume().then((vol) => console.log("Volume: %d", vol))
                     break
-                case 2002:
-                    console.log("Rotate clockwise - Short release")
-                    break
-                case 3002:
-                    console.log("Rotate counter clockwise - Short release")
-                    break
-                default:
-                    console.log('Unsupported buttonevent %s', evt.state.buttonevent)
             }
         }
         catch (e) {
-            console.log("Error occured evaluating button event.")
-            console.log(e)
+            console.error("Error occured evaluating button event.")
+            console.error(e)
             return
         }
     }
