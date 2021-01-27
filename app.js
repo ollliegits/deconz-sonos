dbcc5670d83af701ef31ea965345e495d92a403a
Oliver <oliver.stache@t-systems.com>
2021-01-27 10:53:43 +0100

    feat: Query Sonos favorites from Zone player.
    Also, adding some verbosity.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const WebSocket = require('ws')
 const { Sonos } = require('sonos')
 
+const verbose = process.env.VERBOSE || false
 
 const deHost = process.env.DECONZ_WS_HOST || '192.168.1.50'
 const dePort = process.env.DECONZ_WS_PORT || 443
@@ -20,6 +21,11 @@ const ws = new WebSocket('ws://' + deHost + ':' + dePort)
 
 console.log('Start listening...')
 
+player.getFavorites().then(favorites => {
+    console.log('Got current favorites %j', favorites)
+}).catch(err => { console.log('Error occurred %j', err) })
+
+
 // IKEA-SYMFONISK-REMOTE https://dresden-elektronik.github.io/deconz-rest-doc/endpoints/sensors/button_events/#ikea-symfonisk-remote
 ws.onmessage = (msg) => {
     try {
@@ -30,46 +36,52 @@ ws.onmessage = (msg) => {
         return
     }
 
-    if (evt && evt.e == 'changed' && evt.id == switch_id) {
-        try {
-            switch (evt.state.buttonevent) {
-                case 1002:
-                    console.log("Toggle playback")
-                    player.togglePlayback()
-                    break
-                case 1004:
-                    console.log("Next title in queue")
-                    player.next()
-                    break
-                case 1005:
-                    console.log("Previous title in queue")
-                    player.previous()
-                    break
-                case 2001:
-                    tsClockwiseStart = Date.now()
-                    break
-                case 2003:
-                    duration = Date.now() - tsClockwiseStart
-                    console.log("volume ∂: %d", duration / zpVolFactor)
-                    player.adjustVolume(duration / zpVolFactor)
-                    player.getVolume().then((vol) => console.log("Volume: %d", vol))
-                    break
-                case 3001:
-                    tsCounterClockwiseStart = Date.now()
-                    break
-                case 3003:
-                    duration = Date.now() - tsCounterClockwiseStart
-                    console.log("volume ∂: %d", -1 * duration / zpVolFactor)
-                    player.adjustVolume(-1 * duration / zpVolFactor)
-                    player.getVolume().then((vol) => console.log("Volume: %d", vol))
-                    break
+    if (evt && evt.e == 'changed') {
+        if (verbose == 'true') {
+            console.log(evt)
+        }
+        else
+            if (evt.id == switch_id) {
+                try {
+                    switch (evt.state.buttonevent) {
+                        case 1002:
+                            console.log("Toggle playback")
+                            player.togglePlayback()
+                            break
+                        case 1004:
+                            console.log("Next title in queue")
+                            player.next()
+                            break
+                        case 1005:
+                            console.log("Previous title in queue")
+                            player.previous()
+                            break
+                        case 2001:
+                            tsClockwiseStart = Date.now()
+                            break
+                        case 2003:
+                            duration = Date.now() - tsClockwiseStart
+                            console.log("volume ∂: %d", duration / zpVolFactor)
+                            player.adjustVolume(duration / zpVolFactor)
+                            player.getVolume().then((vol) => console.log("Volume: %d", vol))
+                            break
+                        case 3001:
+                            tsCounterClockwiseStart = Date.now()
+                            break
+                        case 3003:
+                            duration = Date.now() - tsCounterClockwiseStart
+                            console.log("volume ∂: %d", -1 * duration / zpVolFactor)
+                            player.adjustVolume(-1 * duration / zpVolFactor)
+                            player.getVolume().then((vol) => console.log("Volume: %d", vol))
+                            break
+                    }
+                }
+                catch (e) {
+                    console.error("Error occured evaluating button event.")
+                    console.error(e)
+                    console.error(evt)
+                    return
+                }
             }
-        }
-        catch (e) {
-            console.error("Error occured evaluating button event.")
-            console.error(e)
-            console.error(evt)
-            return
-        }
     }
 }
